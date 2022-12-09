687a74ea7b9fef94141da287be49f5eec89ad2aa
fstqwq <fstqwq@foxmail.com>
2022-12-10 07:21:00 +0800

    second commit: a useful version now. pretty table with concerned solving status. very slow, since no cache

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
 from flask import Flask, render_template, request, url_for
-import requests
+import requests, time, json
 from datetime import datetime, timedelta
+from jinja2.filters import FILTERS
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+def color_from_rank(rank):
+    if rank is None:
+        return ""
+    if rank == "legendary grandmaster":
+        return "rated-user user-legendary"
+    if "grandmaster" in rank:
+        return "rated-user user-red"
+    if rank == "candidate master":
+        return "rated-user user-violet"
+    if "master" in rank:
+        return "rated-user user-orange"
+    if rank == "expert":
+        return "rated-user user-blue"
+    if rank == "specialist":
+        return "rated-user user-cyan"
+    if rank == "pupil":
+        return "rated-user user-green"
+    if rank == "newbie":
+        return "rated-user user-gray"
+    return ""
+
+def color_from_rating(rating):
+    if rating >= 3000:
+        return "rated-user user-legendary"
+    if rating >= 2400:
+        return "rated-user user-red"
+    if rating >= 2100:
+        return "rated-user user-orange"
+    if rating >= 1900:
+        return "rated-user user-violet"
+    if rating >= 1600:
+        return "rated-user user-blue"
+    if rating >= 1400:
+        return "rated-user user-cyan"
+    if rating >= 1200:
+        return "rated-user user-green"
+    if rating >= -1000:
+        return "rated-user user-gray"
+    return ""
+
+FILTERS['color_from_rank'] = color_from_rank
+FILTERS['color_from_rating'] = color_from_rating
 
 @app.route('/')
 def index_page():
@@ -14,8 +57,12 @@ def index_page():
 @app.route('/query', methods=['POST'])
 def submit():
     cf_id = request.form['cf']
-    at_id = request.form['at']
-    return render_template('query.html', table_html=request_codeforces(cf_id))
+    concerned = cf_id.split(';')[0]
+    return render_template('query.html', contests=request_codeforces(cf_id), rated_color=cf_community_rated_color(cf_id), concerned = concerned)
+
+@app.route('/faq', methods=['GET'])
+def faq():
+    return render_template('faq.html')
 
 def create_table_html(data):
     html = '<table class="table table-striped table-bordered table-hover">\n'
@@ -57,24 +104,87 @@ def create_table_html(data):
 def request_codeforces(id):
     if id is None or len(id) < 1:
         return None
-    url = 'https://codeforces.com/api/user.status'
+    concerned = id.split(';')[0]
+
+    # First, get a list of all available contests
+    url = 'https://codeforces.com/api/contest.list'
     params = {
-        'handle': id,
-        'from': 1,
-        'count': 10,
-        'key': 'CF_API_KEY',
-        'secret': 'CF_API_SECRET'
+        'gym': False,
     }
     response = requests.get(url, params=params)
     response_data = response.json()
     status = response_data['status']
     if status != 'OK':
         return status
-    
-    # Create HTML table
-    html = create_table_html(response_data['result'])
-    return html
 
+    # Sort the list of contests in reverse chronological order
+    contests = sorted(response_data['result'], key=lambda x: x['startTimeSeconds'], reverse=True)
+
+    contests = [c for c in contests if c['phase'] == 'FINISHED' and c['type'] == 'CF']
+
+    # Get the recent 10 contests that are not in gym
+    recent_contests = contests[:30]
+
+    for contest in recent_contests:
+        url = 'https://codeforces.com/api/contest.standings'
+        if contest['phase'] != 'FINISHED':
+            contest['query_status'] = 'Contest is at phase ' + contest['phase']
+        else:
+            params = {
+                'contestId': contest['id'],
+                'from': 1,
+                'count': 10,
+                'handles': id,
+                'showUnofficial': True
+            }
+            response = requests.get(url, params=params)
+            response_data = response.json()
+            status = response_data['status']
+            if status != 'OK':
+                contest['query_status'] = 'Query return: ' + str(response_data)
+            else:
+                contest['query_status'] = 'OK'
+                result = response_data['result']
+                for row in result['rows']:
+                    if concerned in [m['handle'] for m in row['party']['members']]:
+                        for problem, results in zip(result['problems'], row['problemResults']):
+                            if results['points'] > 0:
+                                problem['concerned_accepted'] = True
+                            elif results['rejectedAttemptCount'] > 0:
+                                problem['concerned_attemped'] = True
+                ac, rj, all = 0, 0, 0
+                for problem in result['problems']:
+                    if 'concerned_accepted' in problem.keys():
+                        ac += 1
+                    elif 'concerned_attemped' in problem.keys():
+                        rj += 1
+                    all += 1
+                contest['concerned_status'] = 'AK' if ac == all else 'OPEN' if ac > 0 else 'RJ' if rj > 0 else 'NEW'
+                contest['query_result'] = result
+    print(recent_contests)
+    return recent_contests
+
+def cf_community_rated_color(handles):
+    base_url = 'https://codeforces.com/api/user.info'
+    params = {
+        'handles': handles
+    }
+
+    response = requests.get(base_url, params=params)
+    response_data = response.json()
+
+    # Check if the request was successful
+    if response_data['status'] != 'OK':
+        raise Exception('Failed to retrieve user information')
+
+    # Extract the rank of each user from the response
+    users = response_data['result']
+    ranks = {}
+    for user in users:
+        color = color_from_rank(user['rank'])
+        ranks[user['handle']] = color
+
+    return ranks
 
 if __name__ == '__main__':
     app.run()
